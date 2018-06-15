a0376f9dc6ea11d95b2c8afbdf1bce8833b582bb
NateA275 <nateaurich@gmail.com>
2018-06-15 10:25:22 -0700

    Initial Setup

---
+++ LINQ_In_Manhatten/LINQ_In_Manhatten/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LINQ_In_Manhatten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
